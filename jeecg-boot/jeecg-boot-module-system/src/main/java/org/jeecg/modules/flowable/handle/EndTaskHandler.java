3babe62ac2b0421d9842e8db24d19adf8409e220
penglee <penglee163@163.com>
2019-12-23 19:47:30 +0800

    提交结束节点直接重置单据状态

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/flowable/handle/EndTaskHandler.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/flowable/handle/EndTaskHandler.java
@@ -1,26 +1,26 @@
 package org.jeecg.modules.flowable.handle;
-import org.flowable.engine.delegate.TaskListener;
-import org.flowable.task.service.delegate.DelegateTask;
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
 import org.jeecg.modules.edusport.entity.DormAthleteLeave;
 import org.jeecg.modules.edusport.service.IDormAthleteLeaveService;
 import org.jeecg.modules.edusport.service.impl.DormAthleteLeaveServiceImpl;
 import org.jeecg.modules.flowable.util.SpringUtil;
 
-public class EndTaskHandler implements TaskListener {
+public class EndTaskHandler  implements JavaDelegate{
  
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+
 
 	@Override
-    public void notify(DelegateTask delegateTask) {
+	public void execute(DelegateExecution execution) {
+		System.out.println("EndTaskHandler已经执行已经执行！");
+		
 		IDormAthleteLeaveService dormAthleteLeaveService = SpringUtil.getObject(DormAthleteLeaveServiceImpl.class);
-		String billId = "" + delegateTask.getVariable("billId");
+		String billId = "" + execution.getVariable("billId");
 		System.out.println(">>>>>>>>>>billId>>>>>>>>>>>>>" + billId);
 		DormAthleteLeave dormAthleteLeave = dormAthleteLeaveService.getById(billId);
 		dormAthleteLeave.setWorkflowState("3");
 		dormAthleteLeaveService.saveOrUpdate(dormAthleteLeave);
-    }
+		
+	}
  
 }
