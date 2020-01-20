59da179bf8173bcadba349e5f4071404257ed252
yanghongliang <yanghongliang@ikeyleap.com>
2020-01-20 20:37:11 +0800

    运动员选择调整 Signed-off-by: yanghongliang <yanghongliang@ikeyleap.com>

--- ant-design-vue-jeecg/src/views/edusport/modules/AthleteSelectionTestModal.vue
+++ ant-design-vue-jeecg/src/views/edusport/modules/AthleteSelectionTestModal.vue
@@ -25,12 +25,12 @@
           <j-search-select-tag v-decorator="['groupId', validatorRules.groupId]" dict="tb_edu_athlete_selection_group,group_name,id" />
         </a-form-item>
         <a-form-item label="运动员" :labelCol="labelCol" :wrapperCol="wrapperCol">
-          <!-- <j-multi-select-tag type="list_multi" v-decorator="['athleteNos']" :trigger-change="true" dictCode="tb_edu_athlete,athlete_name,athlete_no" placeholder="请选择运动员"/> -->
-          <j-multi-select-tag type="checkbox" v-decorator="['athleteNos', validatorRules.athleteNos]" :trigger-change="true" dictCode="tb_edu_athlete,athlete_name,athlete_no" placeholder="请选择运动员"/>
+          <j-multi-select-tag type="list_multi" v-decorator="['athleteNos']" :trigger-change="true" dictCode="tb_edu_athlete,athlete_name,athlete_no" placeholder="请选择运动员"/>
+          <!-- <j-multi-select-tag type="checkbox" v-decorator="['athleteNos', validatorRules.athleteNos]" :trigger-change="true" dictCode="tb_edu_athlete,athlete_name,athlete_no" placeholder="请选择运动员"/> -->
         </a-form-item>
         <a-form-item label="指标" :labelCol="labelCol" :wrapperCol="wrapperCol">
-          <!-- <j-multi-select-tag type="list_multi" v-decorator="['indexCodes']" :trigger-change="true" dictCode="tb_edu_athlete_selection_index,cn_name,l3_code" placeholder="请选择指标"/> -->
-          <j-multi-select-tag type="checkbox" v-decorator="['indexCodes']" :trigger-change="true" dictCode="tb_edu_athlete_selection_index,cn_name,l3_code" placeholder="请选择指标"/>
+          <j-multi-select-tag type="list_multi" v-decorator="['indexCodes']" :trigger-change="true" dictCode="tb_edu_athlete_selection_index,cn_name,l3_code" placeholder="请选择指标"/>
+          <!-- <j-multi-select-tag type="checkbox" v-decorator="['indexCodes']" :trigger-change="true" dictCode="tb_edu_athlete_selection_index,cn_name,l3_code" placeholder="请选择指标"/> -->
         </a-form-item>
         <a-form-item label="状态" :labelCol="labelCol" :wrapperCol="wrapperCol">
           <j-dict-select-tag type="list" v-decorator="['billState']" :trigger-change="true" dictCode="bill_state" placeholder="请选择状态"/>
